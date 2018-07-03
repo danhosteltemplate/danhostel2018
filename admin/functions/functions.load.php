9d4eeed8c833de2f7f1e4dc6f6fa9dc31e3c9b60
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2018-07-03 11:46:22 +0300

    commention out line 10
    empty file troubles

--- admin/functions/functions.load.php
+++ admin/functions/functions.load.php
@@ -7,7 +7,7 @@
  * @since       1.4.0
  * @author      Syamil MJ
  */
-require_once( ADMIN_PATH . 'functions/functions.php' );
+//require_once( ADMIN_PATH . 'functions/functions.php' );
 require_once( ADMIN_PATH . 'functions/functions.interface.php' );
 require_once( ADMIN_PATH . 'functions/functions.options.php' );
 require_once( ADMIN_PATH . 'functions/functions.admin.php' );
