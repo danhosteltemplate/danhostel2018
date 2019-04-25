ec4d8d24b13341cb8c59fea4d164a3eb1b5236cd
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2019-04-25 18:42:30 +0300

    added cookie bar + mobile sub menu fix

--- footer.php
+++ footer.php
@@ -1,40 +1,49 @@
+<div class="fullblue">
+	<div class="">
 
+		<!-- BEGIN #footer-bottom -->
+		<div id="footer-bottom" class="clearfix">
 
-                <div class="fullblue">
-                	<div class="">
-
-					<!-- BEGIN #footer-bottom -->
-					<div id="footer-bottom" class="clearfix">
-
-						<p style="text-align:center">
-							<?php // Display footer message
+			<p style="text-align:center">
+				<?php // Display footer message
 
 									_e('<a href="http://www.danhostel.dk" style="text-align:center">&copy; Copyright www.danhostel.dk</a>','qns');
 							?> - <a href="https://www.danhostel.dk/persondatapolitik" class="footer-terms">Danhostels persondatapolitik</a>
-						</p>
+			</p>
 
-					<!-- END #footer-bottom -->
-					</div>
-
-				</div>
-            </div>
-
-		<!-- END .background-wrapper -->
+			<!-- END #footer-bottom -->
 		</div>
+
+	</div>
+</div>
+
+<!-- END .background-wrapper -->
+</div>
 
 
 <style>
-    @media all and (max-width: 767px){
-    form.booking-form.booking-validation.hidden{
-        display:block !important;
-    }
-    form.booking-form.booking-validation{
-        display:none !important;
-    }
-    }
-</style>
-	<?php wp_footer(); ?>
+	@media all and (max-width: 767px) {
+		form.booking-form.booking-validation.hidden {
+			display: block !important;
+		}
 
-	<!-- END body -->
-	</body>
+		form.booking-form.booking-validation {
+			display: none !important;
+		}
+	}
+</style>
+<?php wp_footer(); ?>
+
+<div id="gdpr-box" class="cookies-accept">
+	<div class="gdcontent">
+		<h2>Vi anvender cookies til at forbedre oplevelsen på vores side.</h2>
+		<p>Du accepterer ved at fortsætte</p>
+	</div>
+	<button class="gdpr-button-accept">OK, jeg accepterer</button>
+</div>
+
+
+<!-- END body -->
+</body>
+
 </html>
