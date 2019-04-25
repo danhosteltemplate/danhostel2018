43fea1f170f80dd098e23932bd3d9bfd27c1019d
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2019-04-25 18:43:46 +0300

    added cookie bar + mobile sub menu fix

--- js/scripts.js
+++ js/scripts.js
@@ -67,10 +67,15 @@ jQuery(function() {
          "text"    : "Menu"
       }).appendTo(".nav-wrapper select");
       jQuery("#mobile-menu a").each(function() {
-       var el = jQuery(this);
+	   var el = jQuery(this);
+	   if(el.parent().parent().hasClass('sub-menu')){
+		   var prefix = '— ';
+	   } else {
+		var prefix = '';
+	   }
        jQuery("<option />", {
            "value"   : el.attr("href"),
-           "text"    : el.text()
+           "text"    : prefix + el.text()
        }).appendTo(".nav-wrapper select");
       });
       jQuery(".nav-wrapper select").change(function() {
@@ -312,3 +317,22 @@ jQuery(window).load(function(){
       window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer', windowFeatures);
       return false;
   }
+
+  jQuery(document).ready(function() {
+	// This will make the cookie bar disappear when the accept button is clicked
+		jQuery('.gdpr-button-accept').click(function(){
+			jQuery('#gdpr-box').fadeOut('slow');
+			
+			if (!jQuery('.cookies-accept').is()) {
+				
+				// Add the cookie. It will remain for a year
+				  jQuery.cookie('cookiebar', 1, {expires: 365, path: '/'});
+				}
+			return false;
+		});
+	
+		// If the cookie is true, don't show the cookie bar
+		if (!jQuery.cookie('cookiebar') == 1) {
+			jQuery('.cookies-accept').css({'display':'block'});
+		}
+	});
