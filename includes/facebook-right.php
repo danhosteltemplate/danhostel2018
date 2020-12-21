ef3898cbe4bc26ebfca7910bd9634cc7d83c48e1
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2020-12-21 09:50:18 +0200

    Update facebook-right.php

--- includes/facebook-right.php
+++ includes/facebook-right.php
@@ -27,10 +27,10 @@ switch(ICL_LANGUAGE_CODE){
 		<h2 class="page-title"><strong><?php echo get_bloginfo(); echo ' Facebook '; echo _e($wall,$DOMAIN); ?></strong></h2>
 			<div class="undertitle bordertop2px" style="width:100%; height:13px;"></div>
 			
-			<div style="<?php if( $data['facebookForside']!="Ja" ) {?>width: 250px; padding: 0 5px 10px 0;<?php } else {?>width:100%; <?php } ?> margin: 0px auto; display: block;"
+			<div style="<?php if( get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option')!="Ja" ) {?>width: 250px; padding: 0 5px 10px 0;<?php } else {?>width:100%; <?php } ?> margin: 0px auto; display: block;"
 			
 			
-			class="fb-page" data-href="<?php _e($data['social_facebook'],'qns'); ?>" data-tabs="timeline" data-width="<?php if( $data['facebookForside']!="Ja" ) {?>280px<?php } else {?>500px<?php } ?>" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+			class="fb-page" data-href="<?php the_field('facebook','option'); ?>" data-tabs="timeline" data-width="<?php if( get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option')!="Ja" ) {?>280px<?php } else {?>500px<?php } ?>" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
 			
 	</div>
 </div>
