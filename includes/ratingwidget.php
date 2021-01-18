f09be5739cd635d51d86e10c9913778f14b72f66
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2021-01-18 14:58:17 +0200

    Update ratingwidget.php

--- includes/ratingwidget.php
+++ includes/ratingwidget.php
@@ -34,7 +34,92 @@ switch(ICL_LANGUAGE_CODE){
 <h2 class="page-title" style="margin-bottom:10px;"><strong class="borderbottom2px" style="padding-bottom:12px;"><?php echo __('Ratings','danhostel');?></strong></h2>
 <div class="undertitle bordertop2px" style="width:100%;"></div>
 
-<iframe src="https://www.relationwise.com/danhostel/iwidget.aspx?hostelid=<?php the_field('rating_id','option'); ?>&size=<?php the_field('skal_boksen_med_ratings_vaere_sma_eller_stor','option'); echo $langvar; ?>" seamless="seamless" style="width:100%;height:<?php if(get_field('skal_boksen_med_ratings_vaere_sma_eller_stor','option')=="big"){echo '400px';} else{echo '140px';} ?>;border:0; overflow-x: hidden; overflow-y:hidden;"> </iframe>
+<?php if(get_field('skal_boksen_med_ratings_vaere_sma_eller_stor','option')=="big"){ ?>
+
+<script>
+
+      var dhWidgetSettings = dhWidgetSettings || [];
+
+ 
+
+      dhWidgetSettings.push({
+
+        hostelId: "<?php the_field('rating_id','option'); ?>",
+
+         htmlId: "dh-widget-average-big",
+
+         size: "big",
+
+      });
+
+      (function (w) {
+
+        if (document.getElementById("dhRatingScript")) return;
+
+        let dhW = document.createElement("script");
+
+        dhW.id = "dhRatingScript";
+
+        dhW.type = "text/javascript";
+
+        dhW.defer = true;
+
+        dhW.src = "https://www.danhostel.dk/ratings-widget-js";
+
+        const s = document.getElementsByTagName("script")[0];
+
+        s.parentNode.insertBefore(dhW, s);
+
+      })(window);
+
+    </script>
+
+    <div id="dh-widget-average-big"></div>
+
+ <?php } else { ?>
+
+<script>
+
+      var dhWidgetSettings = dhWidgetSettings || [];
+
+ 
+
+      dhWidgetSettings.push({
+
+        hostelId: "<?php the_field('rating_id','option'); ?>",
+
+         htmlId: "dh-widget-average-small",
+
+         size: "small",
+
+      });
+
+      (function (w) {
+
+        if (document.getElementById("dhRatingScript")) return;
+
+        let dhW = document.createElement("script");
+
+        dhW.id = "dhRatingScript";
+
+        dhW.type = "text/javascript";
+
+        dhW.defer = true;
+
+        dhW.src = "https://www.danhostel.dk/ratings-widget-js";
+
+        const s = document.getElementsByTagName("script")[0];
+
+        s.parentNode.insertBefore(dhW, s);
+
+      })(window);
+
+    </script>
+
+    <div id="dh-widget-average-small"></div>
+
+<?php } ?>
+
 </div>
 </div>
 
