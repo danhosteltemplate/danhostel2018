1bd5cd77ef7ff76640b09f82466b6ba8a1805234
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2021-02-18 13:33:52 +0200

    Update turistinfo.php

--- includes/turistinfo.php
+++ includes/turistinfo.php
@@ -1,26 +1,28 @@
                     <?php 	 global $data; 
-if( (get_field('er_din_vandrerhjem_kursus_certificeret','option')!="Nej") && (get_field('turist_info','option')!="") && ($data['turistlink']!="") ) { ?>
-             
-						
-                        
-                         <div class="content-body leftfloater">                    
+if( (get_field('skal_boksen_med_turist_info_vises_pa_hjemmesiden','option')!="Nej") && (get_field('turist_info','option')!="") && (get_field('turist_info_url','option')!="") ) { ?>
 
-							<div class="borderino" style="padding-bottom:0px;">
-								<h2 class="page-title" style="margin-bottom:10px;"><strong class="borderbottom2px" style="padding-bottom:12px;"><?php _e( 'Turist info','qns' )?></strong></h2>
-								<div class="undertitle bordertop2px" style="width:100%; height:13px;"></div>
 
-<div class="event-list-wrapper">
 
-																				
-					                </div>
-                                    
-                          </div>    
-                          
-                          						<?php // Get the Thumbnail URL?>
-							<a href="<?php the_field('turist_info_url','option'); ?>"><img src="<?php the_field('turist_info','option'); ?>" alt="" class="prev-image" style="margin-bottom:-6px;"/></a>
-												</div>
+                    <div class="content-body leftfloater">
 
-                               
+                    	<div class="borderino" style="padding-bottom:0px;">
+                    		<h2 class="page-title" style="margin-bottom:10px;"><strong class="borderbottom2px"
+                    				style="padding-bottom:12px;"><?php _e( 'Turist info','qns' )?></strong></h2>
+                    		<div class="undertitle bordertop2px" style="width:100%; height:13px;"></div>
 
-                                                                        <?php } ?>                            
-    
+                    		<div class="event-list-wrapper">
+
+
+                    		</div>
+
+                    	</div>
+
+                    	<?php // Get the Thumbnail URL?>
+                    	<a href="<?php the_field('turist_info_url','option'); ?>"><img
+                    			src="<?php the_field('turist_info','option'); ?>" alt="" class="prev-image"
+                    			style="margin-bottom:-6px;" /></a>
+                    </div>
+
+
+
+                    <?php } ?>
