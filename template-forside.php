9dadebc437f1fa3bbea92524ed2f7732529113aa
danhosteltemplate <40599909+danhosteltemplate@users.noreply.github.com>
2021-02-18 14:30:54 +0200

    Update template-forside.php

--- template-forside.php
+++ template-forside.php
@@ -115,10 +115,45 @@ Template Name: Forside
 <div class="content-body leftfloater booking-banner"><?php if(get_field('booking_banner','option')){ ?><a href="<?php the_field('booking_url','option'); ?>" target="_blank"><img src="<?php the_field('booking_banner','option'); ?>"/></a><?php } ?></div>
 <?php } ?>
 <?php 
-if(get_field('vis_facebook_nyhedsboksen_i_hojre_kolonne','option') =="Nej" && get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option') =="Ja"){
-	load_template( get_template_directory() . '/includes/facebook-right.php' );
-}elseif(get_field('vis_facebook_nyhedsboksen_i_hojre_kolonne','option') =="Ja" && get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option') =="Ja"){echo '<div class="content-body leftfloater"><div class="borderino"><h2>Facebook kan ikke ligge to steder<h2></div></div>';}
-else{}
+if(get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option') =="Ja"){
+	
+	
+	global $data;
+	static $DOMAIN = 'qns';
+	
+	switch(ICL_LANGUAGE_CODE){
+		case 'dk' :
+			$wall = 'Væg';
+		break;
+		
+		case 'de' :
+			$wall = 'Wand';
+		break;
+		
+		case 'en' :
+			$wall = 'Wall';
+		break;
+		default : 
+			$wall = 'Væg';
+		break;
+	}
+	?>   
+	<div class="content-body leftfloater">
+		<div class="borderino">
+			<h2 class="page-title"><strong><?php echo get_bloginfo(); echo ' Facebook '; echo _e($wall,$DOMAIN); ?></strong></h2>
+				<div class="undertitle bordertop2px" style="width:100%; height:13px;"></div>
+				
+				<div style="<?php if( get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option')!="Ja" ) {?>width: 250px; padding: 0 5px 10px 0;<?php } else {?>width:100%; <?php } ?> margin: 0px auto; display: block;"
+				
+				
+				class="fb-page" data-href="<?php the_field('facebook','option'); ?>" data-tabs="timeline" data-width="<?php if( get_field('vis_facebook_nyhedsboksen_midt_pa_forsiden','option')!="Ja" ) {?>280px<?php } else {?>500px<?php } ?>" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+				
+		</div>
+	</div>
+	
+	<?php 
+}else{}
+
 ?>
 <?php 
 if(get_field('skal_nyhedsboksen_vises_pa_forsiden','option') =="Ja"){
@@ -145,9 +180,10 @@ load_template( get_template_directory() . '/includes/nyheder-right.php' );
 				
 			endif;
 
-          	if(get_field('vis_facebook_nyhedsboksen_i_hojre_kolonne','option')=="Nej" && $data['facebookright']=="Ja") { 
+          	if(get_field('vis_facebook_nyhedsboksen_i_hojre_kolonne','option')=="Ja") { 
+				 
           		load_template( get_template_directory() . '/includes/facebook-right.php' ); 
-          	}elseif(get_field('vis_facebook_nyhedsboksen_i_hojre_kolonne','option') =="Ja" && $data['facebookForside'] =="Ja"){echo '<div class="content-body leftfloater"><div class="borderino"><h2>Facebook kan ikke ligge to steder<h2></div></div>';}
+          	}
           	else{}
           	load_template( get_template_directory() . '/includes/ikoner.php' ); 
           	load_template( get_template_directory() . '/includes/ratingwidget.php' );
